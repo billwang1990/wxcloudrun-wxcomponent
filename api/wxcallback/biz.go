735b09184bbec23e2126055f9b79ff21f0676b33
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-08 19:55:04 +0800

    测试

--- api/wxcallback/biz.go
+++ api/wxcallback/biz.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+	"bytes"
+    "fmt"
 
 	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/errno"
 	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/log"
@@ -19,6 +21,48 @@ type wxCallbackBizRecord struct {
 	ToUserName string `json:"ToUserName"`
 	MsgType    string `json:"MsgType"`
 	Event      string `json:"Event"`
+}
+
+func postContent(content  string, token string) {
+	log.Infof("wyq-------// postContent %s", content)
+	    // 定义要发送的JSON数据
+		jsonData := []byte(`{
+			"touser": "wangyaqing",
+			"msgtype": "text",
+			"text": {
+				"content": "\n—————保罗AI客服回复"
+			}
+		}`)
+	
+		// 创建POST请求
+		url := "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=" + token
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Println("发送消息 失败 Error:", err)
+			return
+		}
+	
+		// 设置请求头
+		req.Header.Set("Content-Type", "application/json")
+	
+		// 发送请求
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer resp.Body.Close()
+	
+		// 读取响应数据
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	
+		// 打印响应数据
+		fmt.Println(string(body))
 }
 
 func bizHandler(c *gin.Context) {
@@ -45,6 +89,9 @@ func bizHandler(c *gin.Context) {
 		c.JSON(http.StatusOK, errno.ErrSystemError.WithData(err.Error()))
 		return
 	}
+	mytoken := "78_uCzSfzKazw5oy3atpp546ZuEWQg0GbrCptn6E-Gv09QKR_oNji0-o6ey7u-RxLsR4Loq17LAXxF1d-83uYCZ4IG2ZjNykaBGzRSu3gC6aNGox_JTy9NHo4KgvACvJvNKI3w-5bC3-ihC72p2WPZdAGDGZJ"
+
+	postContent("", mytoken)
 	log.Infof("wyq-------// 转发到用户配置的地址")
 	// 转发到用户配置的地址
 	proxyOpen, err := proxyCallbackMsg("", json.MsgType, json.Event, string(body), c)
