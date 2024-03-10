df858bcb0246c1df2325314240a8305dab41a1d6
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 19:45:30 +0800

    fix bug

--- db/model/talksai.go
+++ db/model/talksai.go
@@ -3,7 +3,7 @@ package model
 
 type TalksAIBot struct {
 	BotID    string   `gorm:"column:botid" json:"botid"`
-	AppID    string   `gorm:"column:botid" json:"appid"`
+	AppID    string   `gorm:"column:appid" json:"appid"`
 	Filters  []string `gorm:"column:filters" json:"filters"`
 	Prefix   string   `gorm:"column:prefix" json:"prefix"`
 	Suffix   string   `gorm:"column:suffix" json:"suffix"`
