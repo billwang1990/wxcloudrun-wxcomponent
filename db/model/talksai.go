f0546a740d47350d3d7bebf02829180ed53c69a1
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 20:19:07 +0800

    更改数据格式

--- db/model/talksai.go
+++ db/model/talksai.go
@@ -1,10 +1,9 @@
 package model
 
-
 type TalksAIBot struct {
-	BotID    string   `gorm:"column:botid" json:"botid"`
-	AppID    string   `gorm:"column:appid" json:"appid"`
-	Filters  []string `gorm:"column:filters" json:"filters"`
-	Prefix   string   `gorm:"column:prefix" json:"prefix"`
-	Suffix   string   `gorm:"column:suffix" json:"suffix"`
+	BotID   string `gorm:"column:botid" json:"botid"`
+	AppID   string `gorm:"column:appid" json:"appid"`
+	Filters string `gorm:"column:filters" json:"filters"`
+	Prefix  string `gorm:"column:prefix" json:"prefix"`
+	Suffix  string `gorm:"column:suffix" json:"suffix"`
 }
