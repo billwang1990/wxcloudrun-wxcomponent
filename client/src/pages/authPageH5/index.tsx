66137c7a234c61d63676e067c0b0a67cc56d20ee
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 13:10:33 +0800

    add logs

--- client/src/pages/authPageH5/index.tsx
+++ client/src/pages/authPageH5/index.tsx
@@ -22,16 +22,20 @@ export default function AuthPageH5() {
             })
             if (resp.data.redirectUrl && resp.data.redirectUrl != "" ) {
                 redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`;
+                console.log("数据库存的", redirectUrl)
             } else {
                 // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数
                 const url = new URL(window.location.href);
                 const redirectUri = url.hash.slice(25)
                 if (redirectUri && redirectUri != '') {
                   redirectUrl = redirectUri;
+                  console.log("链接带的", redirectUrl)
                 }            
             }
             if (resp1.code === 0) {
-                window.location.href = `https://open.weixin.qq.com/wxaopen/safe/bindcomponent?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}#wechat_redirect`
+                setTimeout(() => {       
+                    window.location.href = `https://open.weixin.qq.com/wxaopen/safe/bindcomponent?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}#wechat_redirect`
+                }, 20000);
             }
         }
     }
