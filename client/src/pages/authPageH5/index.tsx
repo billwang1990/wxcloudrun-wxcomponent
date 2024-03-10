5ac0bc5892596eeea19fdcd149232496693d7e3b
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 15:16:44 +0800

    fix bug

--- client/src/pages/authPageH5/index.tsx
+++ client/src/pages/authPageH5/index.tsx
@@ -24,18 +24,16 @@ export default function AuthPageH5() {
                 redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`;
                 console.log("数据库存的", redirectUrl)
             } else {
-                // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数
+                // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数   
                 const url = new URL(window.location.href);
                 const redirectUri = url.hash.slice(25)
                 if (redirectUri && redirectUri != '') {
-                  redirectUrl = redirectUri;
-                  console.log("链接带的", redirectUrl)
-                }            
+                  redirectUrl = `${window.location.origin}/#${routes.redirectPage.path}?redirect_url=${redirectUri}`;
+                  console.log("浏览器带的", redirectUrl)
+                }      
             }
             if (resp1.code === 0) {
-                setTimeout(() => {       
                     window.location.href = `https://open.weixin.qq.com/wxaopen/safe/bindcomponent?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}#wechat_redirect`
-                }, 20000);
             }
         }
     }
