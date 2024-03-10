36986de5d3f50a5fca88e5beb86a309207dac5e9
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 11:19:39 +0800

    调整代码

--- client/src/pages/authPageH5/index.tsx
+++ client/src/pages/authPageH5/index.tsx
@@ -22,7 +22,8 @@ export default function AuthPageH5() {
             })
             if (resp.data.redirectUrl) {
                 redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`;
-            } else {
+            } 
+
                 // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数
                 const urlParams = new URLSearchParams(window.location.search);
                 const urlRedirectUrl = urlParams.get('redirect_url');
@@ -30,7 +31,6 @@ export default function AuthPageH5() {
                     redirectUrl = urlRedirectUrl;
                     console.log("浏览器获取的重定向链接", redirectUrl)
                 }
-            }
             if (resp1.code === 0) {
                 setTimeout(() => {
                     window.location.href = `https://open.weixin.qq.com/wxaopen/safe/bindcomponent?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}#wechat_redirect`
