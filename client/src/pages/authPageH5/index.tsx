4f17ff0d583f1fa569f14d0b7b727746505c514a
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 10:37:43 +0800

    如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数

--- client/src/pages/authPageH5/index.tsx
+++ client/src/pages/authPageH5/index.tsx
@@ -21,8 +21,15 @@ export default function AuthPageH5() {
                 noNeedCheckLogin: true
             })
             if (resp.data.redirectUrl) {
-                redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`
-            }
+                redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`;
+              } else {
+                // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数
+                const urlParams = new URLSearchParams(window.location.search);
+                const urlRedirectUrl = urlParams.get('redirect_url');
+                if (urlRedirectUrl) {
+                  redirectUrl = urlRedirectUrl;
+                }
+              }
             if (resp1.code === 0) {
                 window.location.href = `https://open.weixin.qq.com/wxaopen/safe/bindcomponent?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}#wechat_redirect`
             }
