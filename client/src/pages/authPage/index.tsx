59c20550e2a26f9b871fa8b841fd978e314ff212
billwang1990@gmail.com <billwang1990@gmail.com>
2024-03-10 10:57:46 +0800

    // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数

--- client/src/pages/authPage/index.tsx
+++ client/src/pages/authPage/index.tsx
@@ -20,8 +20,16 @@ export default function AuthPage() {
                 request: getPreAuthCodeRequest,
                 noNeedCheckLogin: true
             })
+            
             if (resp.data.redirectUrl) {
-                redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`
+                redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`;
+            } else {
+                // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数
+                const urlParams = new URLSearchParams(window.location.search);
+                const urlRedirectUrl = urlParams.get('redirect_url');
+                if (urlRedirectUrl) {
+                    redirectUrl = urlRedirectUrl;
+                }
             }
             if (resp1.code === 0) {
                 window.location.href = `https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}`
--- client/src/pages/authPageH5/index.tsx
+++ client/src/pages/authPageH5/index.tsx
@@ -1,7 +1,7 @@
-import {useEffect} from "react";
-import {request} from "../../utils/axios";
-import {getComponentInfoRequest, getPreAuthCodeRequest} from "../../utils/apis";
-import {routes} from "../../config/route";
+import { useEffect } from "react";
+import { request } from "../../utils/axios";
+import { getComponentInfoRequest, getPreAuthCodeRequest } from "../../utils/apis";
+import { routes } from "../../config/route";
 
 export default function AuthPageH5() {
 
@@ -22,14 +22,14 @@ export default function AuthPageH5() {
             })
             if (resp.data.redirectUrl) {
                 redirectUrl = resp.data.redirectUrl.includes(window.location.origin) ? resp.data.redirectUrl : `${window.location.origin}/#${routes.redirectPage.path}`;
-              } else {
+            } else {
                 // 如果 resp.data.redirectUrl 为空，则从当前页面链接获取 redirect_url 参数
                 const urlParams = new URLSearchParams(window.location.search);
                 const urlRedirectUrl = urlParams.get('redirect_url');
                 if (urlRedirectUrl) {
-                  redirectUrl = urlRedirectUrl;
+                    redirectUrl = urlRedirectUrl;
                 }
-              }
+            }
             if (resp1.code === 0) {
                 window.location.href = `https://open.weixin.qq.com/wxaopen/safe/bindcomponent?component_appid=${resp.data.appid}&pre_auth_code=${resp1.data.preAuthCode}&auth_type=6&redirect_uri=${encodeURIComponent(redirectUrl)}#wechat_redirect`
             }
